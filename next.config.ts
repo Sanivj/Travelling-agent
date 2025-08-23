4e512b7c0b655be48a7ddf876eb5a5529988b2b1
Sanivj <himsanivj18@gmail.com>
2025-08-23 12:06:20 +0530

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         hostname: "ovco9b5jyh.ufs.sh",
+      },
+      {
+        hostname: "btawlusa03.ufs.sh",
       },
     ],
   },
